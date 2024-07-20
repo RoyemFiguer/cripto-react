96133aeb77051f78b990b1af989162cde16f7233
RoyemFiguer <rodrigoemfr@hotmail.com>
2024-07-19 18:31:19 -0600

    Component to display errors added

--- src/components/CriptoSearchForm.tsx
+++ src/components/CriptoSearchForm.tsx
@@ -1,8 +1,9 @@
-import {ChangeEvent} from "react"
+import {ChangeEvent, FormEvent} from "react"
 import { useCryptoStore } from "../store"
 import { currencies } from "../data"
 import { useState } from "react"
 import { Pair } from "../types"
+import ErrorMessage from "./ErrorMessage"
 
 export default function CriptoSearchForm() {
 
@@ -11,6 +12,7 @@ export default function CriptoSearchForm() {
         currency: '',
         criptocurrency: ''
     })
+    const [error, setError] = useState('')
 
     const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
         setPair({
@@ -19,14 +21,30 @@ export default function CriptoSearchForm() {
         })
     }
 
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if(Object.values(pair).includes('')){
+            setError('Todos los campos son obligatorios')
+            return
+        }
+        setError('')
+        //Consultar la API
+    }
+
   return (
-    <form className="form">
+    <form 
+        className="form"
+        onSubmit={ handleSubmit }
+    >
+
+        {error && <ErrorMessage>{error}</ErrorMessage>}
         <div className="field">
             <label htmlFor="currency">Moneda:</label>
             <select
                 name="currency"
                 id="currency"
                 onChange={handleChange}
+                value={pair.currency}
             >
                 <option value="">-- Seleccione --</option>
                 {currencies.map(currency => (
@@ -41,6 +59,7 @@ export default function CriptoSearchForm() {
                 name="criptocurrency"
                 id="criptocurrency"
                 onChange={handleChange}
+                value={pair.criptocurrency}
             >
                 <option value="">--Seleccione--</option>
                 {cryptocurrencies.map(crypto => (
